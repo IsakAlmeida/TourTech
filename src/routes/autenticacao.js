c8b28cd0ab7bf93186f981a06859541288beb7e9
IsakAlmeida <isak.almeida@sptech.school>
2026-03-18 18:48:26 -0300

    Preparação para inicio de integração da API
    Manutenção no script de criação
    
    Criação das rotas, controller e models de autenticação
    
    Alteração no app.js para adicionar a rota de autenticacao

---
+++ src/routes/autenticacao.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+var autenticacaoController = require("../controllers/autenticacaoController");
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) {
+    autenticacaoController.cadastrar(req, res);
+})
+
+router.post("/logar", function (req, res) {
+    autenticacaoController.logar(req, res);
+});
+
+module.exports = router;
